bcf8021ec888dcd84da6d297f4dd2fe3f79e5327
Alexandre Lombard <alexandre.lombard@utbm.fr>
2024-01-17 17:42:37 +0100

    Work in progress

--- kotlin-gym/src/main/kotlin/com/kdrl/env/CartPole.kt
+++ kotlin-gym/src/main/kotlin/com/kdrl/env/CartPole.kt
@@ -4,8 +4,20 @@ import com.kdrl.IEnvironment
 import com.kdrl.Step
 import com.kdrl.space.Box
 import com.kdrl.space.Discrete
+import kotlin.math.cos
+import kotlin.math.pow
+import kotlin.math.sin
 
-class CartPole : IEnvironment<FloatArray, Int, Box<FloatArray>, Discrete> {
+class CartPole(
+    val gravity: Float = 9.8f,
+    val massCart: Float = 1.0f,
+    val massPole: Float = 1.0f,
+    val length: Float = 0.5f,
+    val forceMag: Float = 10.0f,
+    val tau: Float = 0.02f) : IEnvironment<FloatArray, Int, Box<FloatArray>, Discrete> {
+
+    val totalMass = massCart + massPole
+    val poleMassLength = massPole + length
 
     override val observationSpace =
         Box<FloatArray>(
@@ -29,6 +41,8 @@ class CartPole : IEnvironment<FloatArray, Int, Box<FloatArray>, Discrete> {
     override fun step(action: Int): Step<FloatArray, Int> {
         var done = false
 
+        // TODO Check if action is valid
+
         // Check termination conditions
         if(state.poleAngle < -12 || state.poleAngle > 12 ||
             state.position < -2.4 || state.position > 2.4 ||
@@ -38,13 +52,26 @@ class CartPole : IEnvironment<FloatArray, Int, Box<FloatArray>, Discrete> {
 
         // Update the state according to the action
         episodeLength += 1
-        var state = this.state
-        var nextState = this.state
+        val state = this.state
+
+        val force = if(action == 1) forceMag else -forceMag
+        val cosTheta = cos(state.poleAngle)
+        val sinTheta = sin(state.poleAngle)
+        val temp = (force + poleMassLength * state.poleAngularVelocity.pow(2) * sinTheta) / totalMass
+        val thetaAcc = (gravity * sinTheta - cosTheta * temp) / (length * (4.0f / 3.0f - massPole * cosTheta.pow(2) / totalMass))
+        val xAcc = temp - poleMassLength * thetaAcc * cosTheta / totalMass
+
+        // Euler update
+        val nextState = InternalState(
+            state.position + tau * state.velocity,
+            state.velocity + tau * xAcc,
+            state.poleAngle + tau * state.poleAngularVelocity,
+            state.poleAngularVelocity + tau * thetaAcc)
 
         this.state = nextState
 
         // Compute the reward
-        val reward = episodeLength.toFloat()
+        val reward = 1.0f
 
         return Step(state.toObservation(), action, nextState.toObservation(), reward, done)
     }
