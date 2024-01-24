156d6cd3b58d8aa020f78bf47eef765adc220f7b
Alexandre Lombard <alexandre.lombard@utbm.fr>
2024-01-24 17:23:49 +0100

    Work in progress

--- kotlin-drl-impl/src/test/kotlin/com/kdrl/dqn/CartPoleDQNTest.kt
+++ kotlin-drl-impl/src/test/kotlin/com/kdrl/dqn/CartPoleDQNTest.kt
@@ -1,11 +1,15 @@
 package com.kdrl.dqn
 
 import com.kdrl.env.CartPole
+import org.apache.commons.math3.geometry.spherical.twod.Vertex
 import org.deeplearning4j.nn.api.OptimizationAlgorithm
 import org.deeplearning4j.nn.conf.BackpropType
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration
+import org.deeplearning4j.nn.conf.graph.ElementWiseVertex
+import org.deeplearning4j.nn.conf.graph.MergeVertex
 import org.deeplearning4j.nn.conf.layers.DenseLayer
 import org.deeplearning4j.nn.conf.layers.OutputLayer
+import org.deeplearning4j.nn.graph.ComputationGraph
 import org.deeplearning4j.nn.weights.WeightInit
 import org.nd4j.linalg.activations.Activation
 import org.nd4j.linalg.api.ops.impl.loss.HuberLoss
@@ -16,6 +20,7 @@ import kotlin.test.Test
 class CartPoleDQNTest {
     @Test
     fun testCartPoleDQN() {
+        // Checked, seems OK
         val innerLayersSize = 128
         val environment = CartPole(maxEpisodeLength = 500)
         val multiLayerConfiguration = NeuralNetConfiguration.Builder()
@@ -37,7 +42,8 @@ class CartPoleDQNTest {
     }
 
     @Test
-    fun testCartPoleDDQN() {
+    fun testCartPoleDoubleDQN() {
+        // Checked, seems OK
         val innerLayersSize = 128
         val environment = CartPole(maxEpisodeLength = 500)
         val multiLayerConfiguration = NeuralNetConfiguration.Builder()
@@ -53,6 +59,34 @@ class CartPoleDQNTest {
             .build()
         val dqn = DQN(environment, multiLayerConfiguration, doubleDqn = true,
             updateTargetModel = DQN.TARGET_UPDATE_BY_POLYAK_AVERAGING(0.9))
+
+        dqn.train(1000) { episodeCount, cumulativeReward ->
+            println("$episodeCount\t$cumulativeReward")
+        }
+    }
+
+    @Test
+    fun testCartPoleDuelingDQN() {
+        val innerLayersSize = 128
+        val environment = CartPole(maxEpisodeLength = 500)
+        val neuralNetConfiguration = NeuralNetConfiguration.Builder()
+            .updater(Adam(1e-4))
+            .weightInit(WeightInit.XAVIER)
+            .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+            .graphBuilder()
+            .addInputs("Input")
+            .addLayer("F1", DenseLayer.Builder().nIn(4).nOut(innerLayersSize).activation(Activation.RELU).build(), "Input")
+            .addLayer("F2", DenseLayer.Builder().nIn(innerLayersSize).nOut(innerLayersSize).activation(Activation.RELU).build(), "F1")
+            .addLayer("V1", DenseLayer.Builder().nIn(innerLayersSize).nOut(1).activation(Activation.IDENTITY).build(), "F2")
+            .addLayer("A1", DenseLayer.Builder().nIn(innerLayersSize).nOut(environment.actionSpace.size).activation(Activation.IDENTITY).build(), "F2")
+            .addVertex("AAvg", ElementWiseVertex(ElementWiseVertex.Op.Average), "A1")
+            .addVertex("A2", ElementWiseVertex(ElementWiseVertex.Op.Subtract), "A1", "AAvg")
+            .addVertex("Q", ElementWiseVertex(ElementWiseVertex.Op.Add), "V1", "A2")
+            .setOutputs("Q")
+            .build()
+
+        val network = ComputationGraph(neuralNetConfiguration)
+        val dqn = DQN(environment, neuralNetConfiguration, doubleDqn = false)
 
         dqn.train(1000) { episodeCount, cumulativeReward ->
             println("$episodeCount\t$cumulativeReward")
