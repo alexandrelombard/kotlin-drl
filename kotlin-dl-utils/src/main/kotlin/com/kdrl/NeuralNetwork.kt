66bce32801067c1fd0d19245a499156a66c497e9
Alexandre Lombard <alexandre.lombard@utbm.fr>
2024-01-24 17:50:09 +0100

    Work in progress

--- kotlin-dl-utils/src/main/kotlin/com/kdrl/NeuralNetwork.kt
+++ kotlin-dl-utils/src/main/kotlin/com/kdrl/NeuralNetwork.kt
@@ -4,6 +4,7 @@ import org.deeplearning4j.nn.api.NeuralNetwork
 import org.deeplearning4j.nn.graph.ComputationGraph
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork
 import org.nd4j.linalg.api.ndarray.INDArray
+import org.nd4j.linalg.factory.Nd4j
 
 sealed class NeuralNetworkWrapper<T: NeuralNetwork>(val network: T) {
     abstract fun fit(data: INDArray, labels: INDArray)
@@ -28,13 +29,11 @@ class NNMultiLayerNetwork(network: MultiLayerNetwork): NeuralNetworkWrapper<Mult
 
 class NNComputationGraph(network: ComputationGraph): NeuralNetworkWrapper<ComputationGraph>(network) {
     override fun fit(data: INDArray, labels: INDArray) {
-        // computationGraph.fit(data, labels)
-        TODO()
+        network.fit(arrayOf(data), arrayOf(labels))
     }
 
     override fun output(input: INDArray): INDArray {
-        // return computationGraph.output(input)
-        TODO()
+         return Nd4j.toFlattened(*network.output(input))
     }
 
     override fun setParams(params: INDArray) {
